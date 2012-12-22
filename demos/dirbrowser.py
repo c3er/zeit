d53b108613be105708b49dfc91c9ffb513b6ef9c
flipperx <flipperx@eefdf0dd-311a-764c-bf8b-e2e7d2181935>
2012-12-22 12:04:32 +0000

    git-svn-id: https://ada/svn/zeit@36 eefdf0dd-311a-764c-bf8b-e2e7d2181935

--- demos/dirbrowser.py
+++ demos/dirbrowser.py
@@ -7,6 +7,16 @@ import glob
 import tkinter
 from tkinter import ttk
 
+def autoscroll(sbar, first, last):
+    """Hide and show scrollbar as needed."""
+    first, last = float(first), float(last)
+    if first <= 0 and last >= 1:
+        sbar.grid_remove()
+    else:
+        sbar.grid()
+    sbar.set(first, last)
+
+# Treeview related #############################################################
 def populate_tree(tree, node):
     if tree.set(node, "type") != 'directory':
         return
@@ -26,12 +36,12 @@ def populate_tree(tree, node):
             ptype = "file"
 
         fname = os.path.split(p)[1]
-        id = tree.insert(node, "end", text=fname, values=[p, ptype])
+        id = tree.insert(node, "end", text = fname, values = [p, ptype])
 
         if ptype == 'directory':
             if fname not in ('.', '..'):
-                tree.insert(id, 0, text="dummy")
-                tree.item(id, text=fname)
+                tree.insert(id, 0, text = "dummy")
+                tree.item(id, text = fname)
         elif ptype == 'file':
             size = os.stat(p).st_size
             tree.set(id, "size", "%d bytes" % size)
@@ -39,9 +49,11 @@ def populate_tree(tree, node):
 
 def populate_roots(tree):
     dir = os.path.abspath('.').replace('\\', '/')
-    node = tree.insert('', 'end', text=dir, values=[dir, "directory"])
+    node = tree.insert('', 'end', text = dir, values = [dir, "directory"])
     populate_tree(tree, node)
+################################################################################
 
+# Handler ######################################################################
 def update_tree(event):
     tree = event.widget
     populate_tree(tree, tree.focus())
@@ -55,20 +67,13 @@ def change_dir(event):
             os.chdir(path)
             tree.delete(tree.get_children(''))
             populate_roots(tree)
+################################################################################
 
-def autoscroll(sbar, first, last):
-    """Hide and show scrollbar as needed."""
-    first, last = float(first), float(last)
-    if first <= 0 and last >= 1:
-        sbar.grid_remove()
-    else:
-        sbar.grid()
-    sbar.set(first, last)
-
+# "Main function" ##############################################################
 root = tkinter.Tk()
 
-vsb = ttk.Scrollbar(orient="vertical")
-hsb = ttk.Scrollbar(orient="horizontal")
+vsb = ttk.Scrollbar(orient = "vertical")
+hsb = ttk.Scrollbar(orient = "horizontal")
 
 tree = ttk.Treeview(
     columns = ("fullpath", "type", "size"),
@@ -96,3 +101,4 @@ root.grid_columnconfigure(0, weight = 1)
 root.grid_rowconfigure(0, weight = 1)
 
 root.mainloop()
+################################################################################
